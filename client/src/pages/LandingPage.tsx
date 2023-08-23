662241dabe94ebf7b77ac023827d0ef6216703b7
Sam Jones <samejones2018@gmail.com>
2023-08-22 20:00:58 -0700

    product page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,44 +1,49 @@
 const LandingPage = () => {
   return (
-    <div className="container mx-auto my-8">
-      <h2 className="text-3xl font-semibold mb-4">Welcome to Our Tech Store</h2>
-      <p className="text-lg mb-4">Explore the latest gadgets and tech products at unbeatable prices.</p>
+    <div className="container mx-auto my-8 p-4">
+      {/* Header Section */}
+      <header className="flex justify-between items-center mb-8">
+        <h1 className="text-4xl font-bold text-blue-600">Tech Store</h1>
+        <nav className="space-x-4">
+          <a href="#" className="text-blue-500 hover:underline">Home</a>
+          <a href="#" className="text-blue-500 hover:underline">Products</a>
+          <a href="#" className="text-blue-500 hover:underline">Contact</a>
+        </nav>
+        <button className="bg-blue-500 text-white p-2 rounded">Sign In</button>
+      </header>
+
+      {/* Banner Section */}
+      <div className="banner bg-blue-200 p-8 text-center rounded mb-8">
+        <h2 className="text-4xl font-bold mb-4">Welcome to Our Tech Store</h2>
+        <p className="text-xl mb-4">Explore the latest gadgets and tech products at unbeatable prices.</p>
+        <button className="bg-blue-500 hover:bg-blue-400 text-white p-2 rounded mt-4">Shop Now</button>
+      </div>
 
       {/* Categories Section */}
       <div className="grid grid-cols-4 gap-4 mb-8">
-        <div className="category bg-gray-100 p-4 text-center">
-          <h3>Laptops</h3>
-        </div>
-        <div className="category bg-gray-100 p-4 text-center">
-          <h3>Smartphones</h3>
-        </div>
-        <div className="category bg-gray-100 p-4 text-center">
-          <h3>Accessories</h3>
-        </div>
-        <div className="category bg-gray-100 p-4 text-center">
-          <h3>Gaming</h3>
-        </div>
+        {/* Add category items here */}
+        {/* ... */}
       </div>
 
       {/* Featured Products Section */}
       <h2 className="text-2xl font-semibold mb-4">Featured Products</h2>
       <div className="grid grid-cols-3 gap-4 mb-8">
-        {/* You can replace this with a map function to render products dynamically */}
-        <div className="product bg-gray-100 p-4">
-          <h3>Product Name</h3>
-          <p>Product Description</p>
-          <p className="font-bold">$ Price</p>
-        </div>
-        <div className="product bg-gray-100 p-4">
-          <h3>Product Name</h3>
-          <p>Product Description</p>
-          <p className="font-bold">$ Price</p>
-        </div>
-        <div className="product bg-gray-100 p-4">
-          <h3>Product Name</h3>
-          <p>Product Description</p>
-          <p className="font-bold">$ Price</p>
-        </div>
+        {/* Render featured products dynamically */}
+        {/* ... */}
+      </div>
+
+      {/* Deals of the Day Section */}
+      <h2 className="text-2xl font-semibold mb-4">Deals of the Day</h2>
+      <div className="grid grid-cols-3 gap-4 mb-8">
+        {/* Render deals of the day dynamically */}
+        {/* ... */}
+      </div>
+
+      {/* Trending Products Section */}
+      <h2 className="text-2xl font-semibold mb-4">Trending Products</h2>
+      <div className="grid grid-cols-3 gap-4 mb-8">
+        {/* Render trending products dynamically */}
+        {/* ... */}
       </div>
 
       {/* Call to Action Section */}
@@ -47,6 +52,21 @@ const LandingPage = () => {
         <p>Join us and find the perfect tech for you!</p>
         <button className="bg-blue-500 hover:bg-blue-400 text-white p-2 rounded mt-4">Shop Now</button>
       </div>
+
+      {/* Footer Section */}
+      <footer className="bg-gray-800 text-white p-8 mt-8">
+        <div className="flex justify-between">
+          <div>
+            <h3 className="text-xl font-bold mb-2">About Us</h3>
+            <p>Information about the company</p>
+          </div>
+          <div>
+            <h3 className="text-xl font-bold mb-2">Contact</h3>
+            <p>Email: support@techstore.com</p>
+            <p>Phone: +1 (123) 456-7890</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,74 +1,92 @@
-import React from 'react';
-import Select from 'react-select';
+import React from "react";
+import Select from "react-select";
 
 const categories = [
-  { value: 'Laptops', label: 'Laptops' },
-  { value: 'Smartphones', label: 'Smartphones' },
-  { value: 'Accessories', label: 'Accessories' },
-  { value: 'Gaming', label: 'Gaming' },
+  { value: "Laptops", label: "Laptops" },
+  { value: "Smartphones", label: "Smartphones" },
+  { value: "Accessories", label: "Accessories" },
+  { value: "Gaming", label: "Gaming" },
 ];
 
 const brands = [
-  { value: 'Apple', label: 'Apple' },
-  { value: 'Samsung', label: 'Samsung' },
-  { value: 'Dell', label: 'Dell' },
-  { value: 'Sony', label: 'Sony' },
-  { value: 'Microsoft', label: 'Microsoft' },
-  { value: 'HP', label: 'HP' },
+  { value: "Apple", label: "Apple" },
+  { value: "Samsung", label: "Samsung" },
+  { value: "Dell", label: "Dell" },
+  { value: "Sony", label: "Sony" },
+  { value: "Microsoft", label: "Microsoft" },
+  { value: "HP", label: "HP" },
 ];
 
 const ProductPage = () => {
   return (
     <div className="container mx-auto my-8 p-4">
       <h2 className="text-4xl font-semibold mb-2">Explore Our Tech Store</h2>
-      <p className="text-lg mb-4">Discover the best in technology with a vast array of products and top brands.</p>
-
-      {/* Search Bar */}
-      <div className="flex mb-6">
-        <input type="text" placeholder="Search..." className="border p-2 flex-grow rounded-l" />
-        <button className="bg-blue-500 text-white p-2 rounded-r">Search</button>
-      </div>
-
+      <p className="text-lg mb-4">
+        Discover the best in technology with a vast array of products and top
+        brands.
+      </p>
       <div className="flex">
         {/* Filters Section */}
         <div className="w-1/4 bg-gray-100 p-6">
           <h3 className="text-xl font-semibold mb-4">Filters</h3>
-
           <div className="mb-4">
             <label className="block text-lg mb-2">Category:</label>
             <Select options={categories} isMulti />
           </div>
-
           <div className="mb-4">
             <label className="block text-lg mb-2">Price Range:</label>
-            <input type="range" className="w-full" />
+            <input type="range" className="w-full mb-2" />
+            <div className="flex justify-between">
+              <input type="number" placeholder="Min Price" className="border p-2 rounded-l" />
+              <input type="number" placeholder="Max Price" className="border p-2 rounded-r" />
+            </div>
           </div>
-
-          <div className="mb-4">
-            <label className="block text-lg mb-2">Brand:</label>
-            <Select options={brands} isMulti />
-          </div>
-
           <div className="mb-4">
             <label className="block text-lg mb-2">Rating:</label>
-            <input type="range" className="w-full" min="1" max="5" step="0.5" />
+            {[1, 2, 3, 4, 5].map((star) => (
+              <label key={star}>
+                <input type="radio" name="rating" value={star} />
+                {' '.repeat(star)}{' '}
+                {'\u2605'.repeat(star) + '\u2606'.repeat(5 - star)}
+                <br />
+              </label>
+            ))}
           </div>
         </div>
-
         {/* Products Grid */}
         <div className="w-3/4 grid grid-cols-4 gap-4 pl-6">
-          {/* Example Product */}
-          <div className="product bg-gray-100 p-4">
-            <img src="image_url" alt="product" className="w-full h-32 object-cover mb-2" />
-            <h3>Product Name</h3>
-            <p>Product Description</p>
-            <p className="font-bold">$ Price</p>
-            <button className="bg-blue-500 text-white p-2 rounded mt-2 w-full">
-              Add to Cart
-            </button>
-          </div>
-          {/* Repeat or map through products */}
+          {Array.from({ length: 16 }).map((_, index) => (
+            <div key={index} className="product bg-gray-100 p-4">
+              <img
+                src="placeholder_image_url"
+                alt="product"
+                className="w-full h-32 object-cover mb-2"
+              />
+              <h3>Product Name</h3>
+              <div className="flex justify-between items-center">
+                <p className="font-bold">$ Price</p>
+                <span className="text-yellow-400">
+                  &#9733; &#9733; &#9733; &#9733; &#9734;
+                </span>
+              </div>
+              <p>Product Description</p>
+              <button className="bg-blue-500 text-white p-2 rounded mt-2 w-full">
+                Add to Cart
+              </button>
+            </div>
+          ))}
         </div>
+      </div>
+      {/* Pagination Section */}
+      <div className="flex justify-center my-6">
+        <button className="bg-blue-500 text-white p-2 rounded mx-2">1</button>
+        <button className="bg-blue-100 text-blue-500 p-2 rounded mx-2">
+          2
+        </button>
+        <button className="bg-blue-100 text-blue-500 p-2 rounded mx-2">
+          3
+        </button>
+        {/* More pages */}
       </div>
     </div>
   );
