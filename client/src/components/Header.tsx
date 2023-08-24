d6cf3848e669a9f95ced0f6194d2fa93cb1426bc
Sam Jones <samejones2018@gmail.com>
2023-08-24 13:40:47 -0700

    hamburger now works in header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'; // Removed useContext
-import SignInModal from './SignInModal';
-import Register from './Register';
-import CartModal from './CartModal';
+import React, { useState } from "react";
+import SignInModal from "./SignInModal";
+import Register from "./Register";
+import CartModal from "./CartModal";
 
 const Header = () => {
   const [signInIsOpen, setSignInIsOpen] = useState(false);
   const [isRegistering, setIsRegistering] = useState(false);
   const [cartIsOpen, setCartIsOpen] = useState(false);
+  const [menuIsOpen, setMenuIsOpen] = useState(false); // State for the hamburger menu
 
-  // Retrieve the user object from localStorage or global state
-  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
 
   const openSignIn = () => setSignInIsOpen(true);
   const closeSignIn = () => setSignInIsOpen(false);
@@ -36,48 +36,69 @@ const Header = () => {
 
   const cartItems: CartItem[] = [
     {
-      id: '1',
-      title: 'Gadget A',
+      id: "1",
+      title: "Gadget A",
       price: 100,
       quantity: 2,
-      imageUrl: 'https://via.placeholder.com/150x100',
+      imageUrl: "https://via.placeholder.com/150x100",
     },
   ];
 
+  const toggleMenu = () => setMenuIsOpen(!menuIsOpen);
+
   return (
     <header className="bg-blue-600 text-white py-4">
-      <div className="container mx-auto flex justify-between items-center">
+    <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <a href="/" className="mr-4">
-            <img src="../assets/images/logo.png" alt="Gadget Logo" className="w-10 h-10" />
+            <img
+              src="../assets/images/logo.png"
+              alt="Gadget Logo"
+              className="w-10 h-10"
+            />
           </a>
           <h1 className="text-2xl font-bold">Gadget</h1>
         </div>
 
-        <nav className="hidden md:flex space-x-4">
-          {/* ... */}
-        </nav>
+        <nav className="hidden md:flex space-x-4">{/* ... */}</nav>
 
-        <div className="flex items-center space-x-4">
-          {user.firstName && <span className="mr-2 text-xl">Hi {user.firstName}!</span>}
-          {user.firstName ? (
-            <button className="bg-blue-800 hover:bg-blue-900 py-2 px-4 rounded">
-              {/* Hamburger icon for Menu */}
+        <div className="flex items-center relative space-x-4"> {/* Add space between items */}
+        {user.firstName && <span className="mr-4 text-xl">Hi {user.firstName}!</span>} {/* Increase margin right */}
+        {user.firstName ? (
+          <>
+            <button onClick={toggleMenu} className="bg-blue-800 hover:bg-blue-900 py-2 px-4 rounded">
               &#9776;
             </button>
-          ) : (
-            <button onClick={openSignIn} className="bg-blue-800 hover:bg-blue-900 py-2 px-4 rounded">
-              Sign In
-            </button>
-          )}
-          <a href="#" className="hover:underline" onClick={openCart}>
-            Cart (0)
-          </a>
-        </div>
+            {menuIsOpen && (
+              <div className="absolute bg-white text-black right-0 top-full mt-2 w-48 rounded-lg shadow-lg z-50">
+                <a href="/profile" className="block p-2 hover:bg-gray-200">Profile</a>
+                <a href="/orders" className="block p-2 hover:bg-gray-200">Orders</a>
+                <a href="/settings" className="block p-2 hover:bg-gray-200">Settings</a>
+                <a href="/logout" className="block p-2 hover:bg-gray-200">Logout</a>
+              </div>
+            )}
+          </>
+        ) : (
+          <button onClick={openSignIn} className="bg-blue-800 hover:bg-blue-900 py-2 px-4 rounded">
+            Sign In
+          </button>
+        )}
+        <a href="#" className="hover:underline ml-4" onClick={openCart}> {/* Increase margin left */}
+          Cart (0)
+        </a>
       </div>
-      {signInIsOpen && !isRegistering && <SignInModal isOpen={signInIsOpen} onClose={closeSignIn} onRegister={handleRegister} />}
+    </div>
+      {signInIsOpen && !isRegistering && (
+        <SignInModal
+          isOpen={signInIsOpen}
+          onClose={closeSignIn}
+          onRegister={handleRegister}
+        />
+      )}
       {isRegistering && <Register onClose={closeRegister} />}
-      {cartIsOpen && <CartModal isOpen={cartIsOpen} onClose={closeCart} items={cartItems} />}
+      {cartIsOpen && (
+        <CartModal isOpen={cartIsOpen} onClose={closeCart} items={cartItems} />
+      )}
     </header>
   );
 };
