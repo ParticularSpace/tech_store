8f56b6295e961b71e8d5e2383e20d9f84d121a39
Sam Jones <samejones2018@gmail.com>
2023-08-30 12:35:05 -0700

    updating product input page

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -10,6 +10,8 @@ const Admin = () => {
     name: Yup.string().required("Product name is required"),
     description: Yup.string().required("Product description is required"),
     price: Yup.number().required("Product price is required"),
+    discountPercentage: Yup.number().min(0).max(100),
+    discountAmount: Yup.number().min(0),
     category: Yup.string().required("Category is required"),
     quantity: Yup.number().required("Quantity is required"),
     stockStatus: Yup.string().required("Stock status is required"),
@@ -21,7 +23,9 @@ const Admin = () => {
     initialValues: {
       name: "",
       description: "",
-      price: 0,
+      price: "0.00",
+      discountPercentage: 0,
+      discountAmount: 0,
       category: "",
       quantity: 0,
       stockStatus: "In Stock",
@@ -30,7 +34,6 @@ const Admin = () => {
     },
     validationSchema,
     onSubmit: (values) => {
-      // TODO: Make a call to the server to add the product
       console.log("Form data", values);
       setProductAdded(true);
     },
@@ -41,113 +44,149 @@ const Admin = () => {
       <h1 className="text-4xl font-semibold mb-8">Admin Dashboard</h1>
       <div className="bg-gray-100 p-8 rounded-lg">
         <h2 className="text-2xl font-semibold mb-6">Add Product</h2>
-        <form onSubmit={formik.handleSubmit} className="space-y-4">
-          
-          <div className="space-y-2">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="grid grid-cols-1 md:grid-cols-2 gap-6"
+        >
+          <div className="space-y-4 col-span-1">
             <h3 className="font-semibold text-lg">Basic Info</h3>
-            <input 
-              type="text" 
-              placeholder="Product Name" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("name")} 
-            />
-            <input 
-              type="text" 
-              placeholder="Description" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("description")} 
-            />
-            <input 
-              type="number" 
-              placeholder="Price" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("price")} 
-            />
-            <input 
-              type="text" 
-              placeholder="Category" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("category")} 
-            />
-            <input 
-              type="number" 
-              placeholder="Quantity" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("quantity")} 
-            />
+            <div className="space-y-2">
+              <input
+                type="text"
+                placeholder="Product Name"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("name")}
+              />
+              <input
+                type="text"
+                placeholder="Description"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("description")}
+              />
+              <input
+                type="text"
+                placeholder="Category"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("category")}
+              />
+              <label className="font-semibold">Price</label>
+              <input
+                type="text"
+                placeholder="Price"
+                className="p-2 rounded border w-full"
+                value={`$${formik.values.price}`}
+                onChange={(e) => {
+                  // Remove non-digit characters and set the value
+                  const value = e.target.value.replace(/[^0-9.]/g, "");
+                  formik.setFieldValue("price", value);
+                }}
+              />
+              <label className="font-semibold">Quantity</label>
+              <input
+                type="number"
+                placeholder="Quantity"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("quantity")}
+              />
+            </div>
           </div>
-          
-          <div className="space-y-2">
-            <h3 className="font-semibold text-lg">Stock and Discounts</h3>
-            <select 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("stockStatus")}
-            >
-              <option value="In Stock">In Stock</option>
-              <option value="Out of Stock">Out of Stock</option>
-              <option value="Discontinued">Discontinued</option>
-            </select>
-            <input 
-              type="number" 
-              placeholder="Discount Percentage" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("discountPercentage")} 
-            />
-            <input 
-              type="number" 
-              placeholder="Discount Amount" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("discountAmount")} 
-            />
+
+          <div className="space-y-4 col-span-1">
+            <h3 className="font-semibold text-lg">Stock</h3>
+            <div className="space-y-2">
+              <select
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("stockStatus")}
+              >
+                <option value="In Stock">In Stock</option>
+                <option value="Out of Stock">Out of Stock</option>
+                <option value="Discontinued">Discontinued</option>
+              </select>
+              <h3 className="font-semibold text-lg">Percent Off</h3>
+              <div className="relative">
+                <input
+                  type="number"
+                  min="0"
+                  max="100"
+                  placeholder="Discount Percentage"
+                  className="p-2 rounded border w-full pr-8" // Updated padding-right
+                  {...formik.getFieldProps("discountPercentage")}
+                />
+                <span className="absolute inset-y-0 right-2 flex items-center text-gray-600">
+                  %
+                </span>
+              </div>
+
+              <h3 className="font-semibold text-lg">Amount Off</h3>
+              <input
+                type="text"
+                placeholder="Discount Amount"
+                className="p-2 rounded border w-full"
+                value={`$${formik.values.discountAmount.toFixed(2)}`}
+                onChange={(e) => {
+                  const value = parseFloat(
+                    e.target.value.replace(/[^0-9.]/g, "")
+                  );
+                  if (!isNaN(value)) {
+                    formik.setFieldValue("discountAmount", value);
+                  }
+                }}
+              />
+            </div>
           </div>
-          
-          <div className="space-y-2">
+
+          <div className="space-y-4 col-span-2">
             <h3 className="font-semibold text-lg">Other Details</h3>
-            <input 
-              type="text" 
-              placeholder="SKU" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("sku")} 
-            />
-            <input 
-              type="text" 
-              placeholder="Manufacturer" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("manufacturer")} 
-            />
-            <input 
-              type="number" 
-              placeholder="Weight" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("weight")} 
-            />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <input
+                type="text"
+                placeholder="SKU"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("sku")}
+              />
+              <input
+                type="text"
+                placeholder="Manufacturer"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("manufacturer")}
+              />
+              <input
+                type="number"
+                placeholder="Weight"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("weight")}
+              />
+            </div>
           </div>
-  
-          <div className="space-y-2">
+
+          <div className="space-y-4 col-span-2">
             <h3 className="font-semibold text-lg">Dimensions</h3>
-            <input 
-              type="number" 
-              placeholder="Length" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("dimensions.length")} 
-            />
-            <input 
-              type="number" 
-              placeholder="Width" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("dimensions.width")} 
-            />
-            <input 
-              type="number" 
-              placeholder="Height" 
-              className="p-2 rounded border" 
-              {...formik.getFieldProps("dimensions.height")} 
-            />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <input
+                type="number"
+                placeholder="Length"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("dimensions.length")}
+              />
+              <input
+                type="number"
+                placeholder="Width"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("dimensions.width")}
+              />
+              <input
+                type="number"
+                placeholder="Height"
+                className="p-2 rounded border w-full"
+                {...formik.getFieldProps("dimensions.height")}
+              />
+            </div>
           </div>
-          
-          <button 
-            type="submit" 
-            className="bg-blue-500 text-white p-2 rounded-lg">
+
+          <button
+            type="submit"
+            className="col-span-2 bg-blue-500 text-white p-2 rounded-lg"
+          >
             Add Product
           </button>
         </form>
@@ -157,8 +196,5 @@ const Admin = () => {
       </div>
     </div>
   );
-  
 };
-
 export default Admin;
-
